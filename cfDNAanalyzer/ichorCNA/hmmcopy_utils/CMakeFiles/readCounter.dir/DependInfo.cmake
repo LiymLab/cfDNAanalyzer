6977b3bb6cdf710194dac182d8483b899e0d0de7
strangesquirrelz <145736225+strangesquirrelz@users.noreply.github.com>
2024-08-22 17:27:04 +0800

    0822
    111

---
+++ cfDNAanalyzer/ichorCNA/hmmcopy_utils/CMakeFiles/readCounter.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/zjp/projects/202312_cfDNA_integrated_tools/ichorCNA/hmmcopy_utils/src/bin/readCounter.cpp" "CMakeFiles/readCounter.dir/src/bin/readCounter.cpp.o" "gcc" "CMakeFiles/readCounter.dir/src/bin/readCounter.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/zjp/projects/202312_cfDNA_integrated_tools/ichorCNA/hmmcopy_utils/lib/CMakeFiles/bamtools.dir/DependInfo.cmake"
+  "/home/zjp/projects/202312_cfDNA_integrated_tools/ichorCNA/hmmcopy_utils/lib/CMakeFiles/split.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cfDNAanalyzer/ichorCNA/hmmcopy_utils/src/util/CMakeFiles/bigWigToWig.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../util/bigwig/bigWigToWig"
+  "../../util/bigwig/bigWigToWig.pdb"
+  "CMakeFiles/bigWigToWig.dir/bigwig/bigWigToWig.c.o"
+  "CMakeFiles/bigWigToWig.dir/bigwig/bigWigToWig.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bigWigToWig.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
