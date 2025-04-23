ad1d79871b72d6f8a06386d7de0065f971115a3a
lymlab-bcb <154315044+LiymLab@users.noreply.github.com>
2025-04-23 21:14:11 +0800

    Delete cfDNAanalyzer directory

--- cfDNAanalyzer/Griffin/Ref_hg38/process.sh
+++
@@ -1,4 +0,0 @@
-for file in sites/*; do
-    filename=$(basename "$file")
-    tail -n +2 "$file" | awk -F '\t' '{print $1 "\t" $2 "\t" $2+1}' > "sites_hg19/$filename"
-done
--- cfDNAanalyzer/ichorCNA/hmmcopy_utils/src/util/CMakeFiles/segToMap.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for segToMap.
-# This may be replaced when dependencies are built.
